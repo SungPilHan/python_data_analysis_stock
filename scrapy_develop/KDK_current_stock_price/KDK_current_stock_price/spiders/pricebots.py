7dffc01e2bcfe98c1a33511d9293cfb4a907a7bc
kimdonggwan337 <76460594+kimdonggwan337@users.noreply.github.com>
2021-03-16 13:42:47 +0900

    코드 업로

--- scrapy_develop/KDK_current_stock_price/KDK_current_stock_price/spiders/pricebots.py
+++ scrapy_develop/KDK_current_stock_price/KDK_current_stock_price/spiders/pricebots.py
@@ -20,21 +20,18 @@ class PricebotsSpider(scrapy.Spider):
 
     def parse(self, response):
         stock_names=response.xpath('//*[@id="middle"]/div[1]/div[1]/h2/a/text()').extract()
-        buying_volume_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[7]/td[1]/span/strong/text()').extract()
-        buying_price_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[7]/td[2]/span/strong/text()').extract()
-        selling_volume_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[11]/td[4]/span/strong/text()').extract()
-        selling_price_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[11]/td[5]/span/strong/text()').extract()
-        
+        buying_volume_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[11]/td[5]/span/strong/text()').extract()
+        buying_price_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[11]/td[4]/span/strong/text()').extract()
+        selling_volume_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[7]/td[1]/span/strong/text()').extract()
+        selling_price_nows=response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr[7]/td[2]/span/strong/text()').extract()
         selling_prices = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[2]/span/text()').extract()
         selling_volumes = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[1]/span/text()').extract()
         buying_prices = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[4]/span/text()').extract()
+        
         buying_volumes = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[5]/span/text()').extract()
-        
         items = []
-        
         print(selling_prices,selling_volumes,buying_prices,buying_volumes)
         
-
         buying_volumes.append(buying_volume_nows[0].strip())
         buying_prices.append(buying_price_nows[0].strip())
         selling_volumes.append(selling_volume_nows[0].strip())
@@ -47,7 +44,6 @@ class PricebotsSpider(scrapy.Spider):
             item['selling_volume'] = selling_volumes[idx].strip()
             item['buying_price'] = buying_prices[idx].strip()
             item['buying_volume'] = buying_volumes[idx].strip()
-           
             items.append(item)
         
         return items
