ab56dedfa6c544ab2c62799468be056121811fac
sohyun2304 <76420030+sohyun2304@users.noreply.github.com>
2021-03-16 16:23:05 +0900

    기관 투자 현황_pipeline 수정
    pipeline

--- scrapy_develop/PSH_institut_stock/PSH_institut_stock/pipelines.py
+++ scrapy_develop/PSH_institut_stock/PSH_institut_stock/pipelines.py
@@ -1,13 +1,19 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
-
+from kafka import KafkaProducer
+from json import dumps
+import time
 class PshInstitutStockPipeline:
+
+    def __init__(self): 
+        self.producer= KafkaProducer( acks=0, compression_type='gzip', bootstrap_servers=['54.144.39.228:9092'],value_serializer=lambda x : dumps(x).encode('utf-8'))      
+
     def process_item(self, item, spider):
+
+        data = {"schema":{"type":"struct","fields":[{"type":"int32","optional":"false","field":"id"},{"type":"string","optional":"false","field":"stock_name"},{"type":"string","optional":"false","field":"stock_date"},{"type":"string","optional":"false","field":"institut_trading_volume"},{"type":"int64","optional":"true","name":"org.apache.kafka.connect.data.Timestamp","version":1,"field":"create_at"}],"optional":"false","name":"institut_table"},"payload":{"id":0,"stock_name":item.get('stock_name','').strip(),"stock_date":item.get('stock_date','').strip(),"institut_trading_volume":item.get('institut_trading_volume','').strip(),"create_at":int(time.time())*1000}}
+        print("wait")
+        time.sleep(0.3)
+        self.producer.send('my_topic_institut_table', value=data)
+        time.sleep(0.3)
+        self.producer.flush()
         return item
+
