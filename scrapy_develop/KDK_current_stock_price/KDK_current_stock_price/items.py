465edba87fc3bbbb4827d080c2b551a7bb5fc4ee
kimdonggwan337 <76460594+kimdonggwan337@users.noreply.github.com>
2021-03-16 10:17:24 +0900

    수정 업로드

--- scrapy_develop/KDK_current_stock_price/KDK_current_stock_price/items.py
+++ scrapy_develop/KDK_current_stock_price/KDK_current_stock_price/items.py
@@ -10,7 +10,7 @@ class KdkCurrentStockPriceItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     buying_volumes= scrapy.Field()
-    buying_price = scrapy.Field()
-    seling_volume= scrapy.Field()
-    seling_price = scrapy.Field()
+    buying_prices = scrapy.Field()
+    selling_volumes= scrapy.Field()
+    selling_prices = scrapy.Field()
     
--- scrapy_develop/KDK_current_stock_price/KDK_current_stock_price/spiders/pricebots.py
+++ scrapy_develop/KDK_current_stock_price/KDK_current_stock_price/spiders/pricebots.py
@@ -1,33 +1,36 @@
 import scrapy
 from KDK_current_stock_price.items import KdkCurrentStockPriceItem
 from scrapy.http import Request
+import os
 
 class PricebotsSpider(scrapy.Spider):
     name = 'pricebots'
     allowed_domains = ['finance.naver.com']
-    start_urls = ['http://https://finance.naver.com/item/sise.nhn?code=005930']
+    start_urls = []
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     def __init__(self):
-        base_url = 'https://finance.naver.com/item/sise.nhn?code={0}&page={1}'
+        base_url = 'http://finance.naver.com/item/sise.nhn?code={0}'
         start_page = 1
-        with open('./stock_list.txt', 'r') as stock_list:
+        with open(self.BASE_DIR + '/stock_list.txt', 'r') as stock_list:
             for stock in stock_list:
                 for p in range(1):
                     temp_url = base_url.format(stock, start_page + p)
                     self.start_urls.append(temp_url)
 
     def parse(self, response):
-        seling_prices = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[2]/span/text()').extract()
-        seling_volumes = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[1]/span/text()').extract()
+        selling_prices = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[2]/span/text()').extract()
+        selling_volumes = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[1]/span/text()').extract()
         buying_prices = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[4]/span/text()').extract()
         buying_volumes = response.xpath('//*[@id="content"]/div[2]/div[2]/table[1]/tbody/tr/td[5]/span/text()').extract()
         items = []
-        print(seling_prices,seling_volumes,buying_prices,buying_volumes)
-        for idx in range(len(seling_volumes)):#len
+        print(selling_prices,selling_volumes,buying_prices,buying_volumes)
+        for idx in range(len(selling_volumes)):#len
             item = KdkCurrentStockPriceItem()
-            item['seling_price'] = seling_prices[idx].strip()
-            item['seling_volume'] = seling_volumes[idx].strip()
+            item['seling_price'] = selling_prices[idx].strip()
+            item['seling_volume'] = selling_volumes[idx].strip()
             item['buying_price'] = buying_prices[idx].strip()
             item['buying_volume'] = buying_volumes[idx].strip()#매수 잔량
             items.append(item)
+        
         return items
