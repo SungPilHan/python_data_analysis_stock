59160020c157d1e13e6d403d69b90e42d81cb863
SungPilHan <ttt16541@gmail.com>
2021-03-17 15:23:31 +0900

    주식 종목 코드 스크래퍼 업로드

---
+++ scrapy_develop/tops/cp_tops.py
@@ -0,0 +1,14 @@
+import os
+import platform
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+if platform.system() == 'Windows':
+    os.system('copy tops.txt {}'.format(BASE_DIR+'\\..\\foreign_stock\\foreign_stock\\spiders\\stock_list.txt'))
+    os.system('copy tops.txt {}'.format(BASE_DIR+'\\..\\HSP_stock_news\\HSP_stock_news\\spiders\\stock_list.txt'))
+    os.system('copy tops.txt {}'.format(BASE_DIR+'\\..\\KDK_current_stock_price\\KDK_current_stock_price\\spiders\\stock_list.txt'))
+    os.system('copy tops.txt {}'.format(BASE_DIR+'\\..\\PSH_institut_stock\\PSH_institut_stock\\spiders\\stock_list.txt'))
+else:
+    os.system('cp tops.txt {}'.format(BASE_DIR+'../foreign_stock/foreign_stock/spiders/stock_list.txt'))
+    os.system('cp tops.txt {}'.format(BASE_DIR+'../HSP_stock_news/HSP_stock_news/spiders/stock_list.txt'))
+    os.system('cp tops.txt {}'.format(BASE_DIR+'../KDK_current_stock_price/KDK_current_stock_price/spiders/stock_list.txt'))
+    os.system('cp tops.txt {}'.format(BASE_DIR+'../PSH_institut_stock/PSH_institut_stock/spiders/stock_list.txt'))
